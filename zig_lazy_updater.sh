524109fe203e537c72ec501cf349c293a0ecc24c
MarioGT <mario@mariogt.com>
2022-08-12 17:47:54 -0400

    added comments, refactoring, Downloads folder detection and more

--- zig_lazy_updater.sh
+++ zig_lazy_updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #  zig_lazy_updater.sh
-#  Created by Mario Gajardo Tassara --> 16-07-2022
+#  By Mario Gajardo Tassara --> 16-07-2022
 #
 #  Zig Lazy Man Installer/Updater
 #
@@ -29,6 +29,8 @@
 #  OUT OF OR IN CONNECTION WITH THE TEXT OR THE USE OR OTHER DEALINGS dtt TEXT.
 
 
+# zig dev release to check
+# here you can mod it to check for windows or mac versions
 zig_dev_release_to_check="zig-linux-x86_64-0.10.0-dev.*.tar.xz"
 
 clear
@@ -36,52 +38,93 @@ echo ""
 echo "🎉 Lazy Man Updater/Installer for Zig dev release 🎉"
 echo ""
 
+# checking if zig is installed and present in your $PATH
 if ! command -v zig &> /dev/null
 then
-    echo "*** I can't find Zig in your Path variables!"
-    echo ""
+	echo "*** I can't find Zig installed"
+	echo ""
 else
-    zig_installed_version=$(zig version)
+    # if zig is installed then we store the local version
+	zig_installed_version=$(zig version)
+
+    # searching for the local version number on the zig website,
+    # if is founded then zig_dev_latest_version var is not nil
     zig_dev_latest_version=$(curl -s https://ziglang.org/download/ | grep -o $zig_installed_version | uniq)
 fi
 
+# checking if is needed to install/update zig
+# if zig_dev_latest_version != nil --> zig installed locally is already at its latest version
+# if zig_dev_latest_version == nil --> zig isn't installed locally or is obsolete
+
 if [ -n "$zig_dev_latest_version" ];
 then
-    echo "🐸 Doing nothing 🐸"
-    echo ""
-    echo "*** You have already the latest dev release!"
-    echo "⚡Zig $zig_dev_latest_version"
+	echo "🐸 Doing nothing 🐸"
+	echo ""
+	echo "*** You have already the latest dev release!"
+	echo "⚡Zig $zig_dev_latest_version"
 else
-    echo "*** New version of Zig dev release found!"
-    echo ""
-    echo "🌎 Downloading the latest dev build ..."
-    cd ~/Downloads
+	echo "*** New version of Zig dev release found!"
+	echo ""
+	echo "🌎 Downloading the latest dev build ..."
 
-    wget -q --no-parent -r --exclude-directories=/documentation,/news,/zsf,/learn,/de,/es,/fr,/it,/ar,/fa,/pt,/zh,/ko,/perf -A $zig_dev_release_to_check https://ziglang.org/
+    # checking if we can use the Downloads folder for temporarily store the downloaded files
+	if [ -e $HOME/.config/user-dirs.dirs ]
+	then
+        # storing the user default Download folder name, in whatever language, with the help of grep and awk
+		downloadFolder=$(cat $HOME/.config/user-dirs.dirs | grep "XDG_DOWNLOAD_DIR" | grep -oP '(?<=/)[^ ]*' | awk '{ print substr( $0, 1, length($0)-1) }')
 
-    mv ziglang.org/builds/zig* ~/ && rm -rf ziglang.org
+        # if the Download folder doesn't exist for whatever reason (like on Windows WSL distros)
+        # create a temp directory for store the downloaded data
+	else
+        if [ -d "zig_temp_download" ]
+        then
+		    downloadFolder="$HOME/zig_temp_download"
+        else
+		    mkdir zig_temp_download
+        fi
+	fi
+	cd $downloadFolder
 
-    echo "✅ Download complete!"
-    echo ""
+    # get the latest zig dev compilation with wget
+	wget -q --no-parent -r --exclude-directories=/documentation,/news,/zsf,/learn,/de,/es,/fr,/it,/ar,/fa,/pt,/zh,/ko,/perf -A $zig_dev_release_to_check https://ziglang.org/
 
-    cd ~/
+    # moving the zig compressed file to your $HOME, then we delete the temp dir $HOME/$downloadFolder
+	mv ziglang.org/builds/zig* $HOME && rm -rf ziglang.org
 
-    if [ -e "zig" ];
+    # deleting the zig_temp_download folder if exist
+    if [ -d zig_temp_download ]
     then
-	    rm -rf zig
-	    echo "-> Old zig installation was found and deleted!"
-        echo ""
+        rm -rf zig_temp_download
     fi
 
-    zig_full_path=$(ls | grep "zig-*")
-    zig_full_file_name=$(basename "$zig_full_path")
-    zig_version=${zig_full_file_name%.*.*}
+	echo "✅ Download complete!"
+	echo ""
 
-    tar -xf zig-*
-    rm zig-*.tar.xz
-    mv ~/zig-* ~/zig
-    echo "⚡$zig_version installed in $HOME/zig"
-    echo ""
-    echo "Add $HOME/zig to your Path, write this line to your shell config file (like .bashrc)"
-    echo 'export PATH="$HOME/zig:$PATH"'
+	cd $HOME
+
+    # check if an old zig installation is present
+    # if exist we zap it!
+	if [ -e "zig" ];
+	then
+		rm -rf zig
+		echo "-> Old zig installation was found and deleted!"
+		echo ""
+	fi
+
+    # storing the zig version number from the downloaded file
+	zig_full_path=$(ls | grep "zig-*")
+	zig_full_file_name=$(basename "$zig_full_path")
+	zig_version=${zig_full_file_name%.*.*}
+
+    # extracting the zig dev compilation
+	tar -xf zig-*
+    # deleting the compressed file
+	rm zig-*.tar.xz
+    # renaming the resulting folder to "zig"
+	mv $HOME/zig-* $HOME/zig
+
+	echo "⚡$zig_version installed in $HOME/zig"
+	echo ""
+	echo "Add $HOME/zig to your Path, write this line to your shell config file (like .bashrc)"
+	echo 'export PATH="$HOME/zig:$PATH"'
 fi
