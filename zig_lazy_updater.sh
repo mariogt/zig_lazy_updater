3d68608c12fd9cab8c863a486186a3346e22cf7e
MarioGT <mario@mariogt.com>
2024-04-23 15:54:57 -0400

    new zig version 13

--- zig_lazy_updater.sh
+++ zig_lazy_updater.sh
@@ -29,163 +29,163 @@
 #  OUT OF OR IN CONNECTION WITH THE TEXT OR THE USE OR OTHER DEALINGS dtt TEXT.
 
 # version number substring, to insert into zig_dev_release_to_check var
-dev_ver_string="0.12.0"
+dev_ver_string="0.13.0"
 
 # updater function
 worker() {
-    # zig dev release to check
-    zig_dev_release_to_check="$1"
+	# zig dev release to check
+	zig_dev_release_to_check="$1"
 
-    # deleting the zig_temp_download folder if exist
-    if [ -d $HOME/zig_temp_download ]; then
-        rm -rf $HOME/zig_temp_download
-    fi
+	# deleting the zig_temp_download folder if exist
+	if [ -d $HOME/zig_temp_download ]; then
+		rm -rf $HOME/zig_temp_download
+	fi
 
-    cd $HOME
+	cd $HOME
 
-    # checking if zig is installed and present in your $PATH
-    if ! command -v zig &>/dev/null; then
-        echo "*** I can't find Zig installed"
-        echo ""
-    else
-        # if zig is installed then we store the local version
-        zig_installed_version=$(zig version)
+	# checking if zig is installed and present in your $PATH
+	if ! command -v zig &>/dev/null; then
+		echo "*** I can't find Zig installed"
+		echo ""
+	else
+		# if zig is installed then we store the local version
+		zig_installed_version=$(zig version)
 
-        # searching for the local version number on the zig website,
-        # if is founded then zig_dev_latest_version var is not nil
-        zig_dev_latest_version=$(curl -s https://ziglang.org/download/ | grep -o $zig_installed_version | uniq)
-    fi
+		# searching for the local version number on the zig website,
+		# if is founded then zig_dev_latest_version var is not nil
+		zig_dev_latest_version=$(curl -s https://ziglang.org/download/ | grep -o $zig_installed_version | uniq)
+	fi
 
-    # checking if is needed to install/update zig
-    # if zig_dev_latest_version != nil --> zig installed locally is already at its latest version
-    # if zig_dev_latest_version == nil --> zig isn't installed locally or is obsolete
+	# checking if is needed to install/update zig
+	# if zig_dev_latest_version != nil --> zig installed locally is already at its latest version
+	# if zig_dev_latest_version == nil --> zig isn't installed locally or is obsolete
 
-    if [ -n "$zig_dev_latest_version" ]; then
-        echo "🐸 Doing nothing 🐸"
-        echo ""
-        echo "*** You have already the latest dev release!"
-        echo "⚡Zig $zig_dev_latest_version"
-    else
-        echo "*** New version of Zig dev release found!"
-        echo ""
-        echo "🌎 Downloading the latest dev build ..."
+	if [ -n "$zig_dev_latest_version" ]; then
+		echo "🐸 Doing nothing 🐸"
+		echo ""
+		echo "*** You have already the latest dev release!"
+		echo "⚡Zig $zig_dev_latest_version"
+	else
+		echo "*** New version of Zig dev release found!"
+		echo ""
+		echo "🌎 Downloading the latest dev build ..."
 
-        # checking if we can use the Downloads folder for temporarily store the downloaded files
-        if [ -e $HOME/.config/user-dirs.dirs ]; then
-            # storing the user default Download folder name, in whatever language, with the help of grep and awk
-            downloadFolder=$HOME/$(cat $HOME/.config/user-dirs.dirs | grep "XDG_DOWNLOAD_DIR" | grep -oP '(?<=/)[^ ]*' | awk '{ print substr( $0, 1, length($0)-1) }')
+		# checking if we can use the Downloads folder for temporarily store the downloaded files
+		if [ -e $HOME/.config/user-dirs.dirs ]; then
+			# storing the user default Download folder name, in whatever language, with the help of grep and awk
+			downloadFolder=$HOME/$(cat $HOME/.config/user-dirs.dirs | grep "XDG_DOWNLOAD_DIR" | grep -oP '(?<=/)[^ ]*' | awk '{ print substr( $0, 1, length($0)-1) }')
 
-            # if the Download folder doesn't exist for whatever reason (like on Windows WSL distros)
-            # create a temp directory for store the downloaded data
-        else
-            if [ -d "$/HOME/zig_temp_download" ]; then
-                downloadFolder="$HOME/zig_temp_download"
-            else
-                mkdir zig_temp_download
-                downloadFolder="$HOME/zig_temp_download"
-            fi
-        fi
-        cd $downloadFolder
+			# if the Download folder doesn't exist for whatever reason (like on Windows WSL distros)
+			# create a temp directory for store the downloaded data
+		else
+			if [ -d "$/HOME/zig_temp_download" ]; then
+				downloadFolder="$HOME/zig_temp_download"
+			else
+				mkdir zig_temp_download
+				downloadFolder="$HOME/zig_temp_download"
+			fi
+		fi
+		cd $downloadFolder
 
-        # get the latest zig dev compilation with wget
-        wget -q --no-check-certificate --no-parent -r --exclude-directories=/documentation,/news,/zsf,/learn,/de,/es,/fr,/it,/ar,/fa,/pt,/zh,/ko,/perf -A $zig_dev_release_to_check https://ziglang.org/
+		# get the latest zig dev compilation with wget
+		wget -q --no-check-certificate --no-parent -r --exclude-directories=/documentation,/news,/zsf,/learn,/de,/es,/fr,/it,/ar,/fa,/pt,/zh,/ko,/perf -A $zig_dev_release_to_check https://ziglang.org/
 
-        # moving the zig compressed file to your $HOME, then we delete the temp dir $HOME/$downloadFolder
-        mv ziglang.org/builds/zig* $HOME && rm -rf ziglang.org
+		# moving the zig compressed file to your $HOME, then we delete the temp dir $HOME/$downloadFolder
+		mv ziglang.org/builds/zig* $HOME && rm -rf ziglang.org
 
-        # deleting the zig_temp_download folder if exist
-        if [ -d $HOME/zig_temp_download ]; then
-            rm -rf $HOME/zig_temp_download
-        fi
+		# deleting the zig_temp_download folder if exist
+		if [ -d $HOME/zig_temp_download ]; then
+			rm -rf $HOME/zig_temp_download
+		fi
 
-        echo "✅ Download complete!"
-        echo ""
+		echo "✅ Download complete!"
+		echo ""
 
-        cd $HOME
+		cd $HOME
 
-        # check if an old zig installation is present
-        # if exist we zap it!
-        if [ -e "zig" ]; then
-            rm -rf zig
-            echo "-> Old zig installation was found and deleted!"
-            echo ""
-        fi
+		# check if an old zig installation is present
+		# if exist we zap it!
+		if [ -e "zig" ]; then
+			rm -rf zig
+			echo "-> Old zig installation was found and deleted!"
+			echo ""
+		fi
 
-        # storing the zig version number from the downloaded file
-        zig_full_path=$(ls | grep "zig-*")
-        zig_full_file_name=$(basename "$zig_full_path")
-        zig_version=${zig_full_file_name%.*.*}
+		# storing the zig version number from the downloaded file
+		zig_full_path=$(ls | grep "zig-*")
+		zig_full_file_name=$(basename "$zig_full_path")
+		zig_version=${zig_full_file_name%.*.*}
 
-        # extracting the zig dev compilation
-        tar -xf zig-*
-        # deleting the compressed file
-        rm zig-*.tar.xz
-        # renaming the resulting folder to "zig"
-        mv $HOME/zig-* $HOME/zig
+		# extracting the zig dev compilation
+		tar -xf zig-*
+		# deleting the compressed file
+		rm zig-*.tar.xz
+		# renaming the resulting folder to "zig"
+		mv $HOME/zig-* $HOME/zig
 
-        echo "⚡$zig_version installed in $HOME/zig"
-        echo ""
-        echo "Add $HOME/zig to your Path, write this line to your shell config file (like .bashrc)"
-        echo 'export PATH="$HOME/zig:$PATH"'
-    fi
+		echo "⚡$zig_version installed in $HOME/zig"
+		echo ""
+		echo "Add $HOME/zig to your Path, write this line to your shell config file (like .bashrc)"
+		echo 'export PATH="$HOME/zig:$PATH"'
+	fi
 }
 
 # main menu function
 mainMenu() {
-    clear
-    echo ""
-    echo "🎉 Lazy Man Updater/Installer for Zig dev release 🎉"
-    echo ""
+	clear
+	echo ""
+	echo "🎉 Lazy Man Updater/Installer for Zig dev release 🎉"
+	echo ""
 
-    bool=true
-    while [ $bool ]; do
-        echo "For what platform you want to install the latest Zig dev release?"
-        echo ""
-        echo "1 = 🐧 Linux (x86_64)"
-        echo "2 = 🍎 macOS (aarch64)"
-        echo "3 = 📺 Windows (x86_64)"
-        echo "4 = exit"
-        echo ""
+	bool=true
+	while [ $bool ]; do
+		echo "For what platform you want to install the latest Zig dev release?"
+		echo ""
+		echo "1 = 🐧 Linux (x86_64)"
+		echo "2 = 🍎 macOS (aarch64)"
+		echo "3 = 📺 Windows (x86_64)"
+		echo "4 = exit"
+		echo ""
 
-        echo "Type your option and press Enter: "
-        read option
-        clear
+		echo "Type your option and press Enter: "
+		read option
+		clear
 
-        if [ "$option" == "1" ]; then
-            bool=false
-            echo "🐧 Linux (x86_64) selected!"
-            echo ""
-            zig_dev_release_to_check="zig-linux-x86_64-$dev_ver_string-dev.*.tar.xz"
-            worker $zig_dev_release_to_check
-            exit
-        elif [ "$option" == "2" ]; then
-            bool=false
-            echo "🍎 macOS (aarch64) selected!"
-            echo ""
-            zig_dev_release_to_check="zig-macos-aarch64-$dev_ver_string-dev.*.tar.xz"
-            worker $zig_dev_release_to_check
-            exit
-        elif [ "$option" == "3" ]; then
-            bool=false
-            echo "📺 Windows (x86_64) selected!"
-            echo ""
-            zig_dev_release_to_check="zig-windows-x86_64-$dev_ver_string-dev.*.tar.xz"
-            worker $zig_dev_release_to_check
-            exit
-        elif [ "$option" == "4" ]; then
-            bool=false
-            clear
-            exit
-        elif [ "$option" == "" ]; then
-            bool=true
-            echo "🔥Empty option! please try again🔥 "
-            echo ""
-        else
-            bool=true
-            echo "🔥Nonvalid option! please try again🔥"
-            echo ""
-        fi
-    done
+		if [ "$option" == "1" ]; then
+			bool=false
+			echo "🐧 Linux (x86_64) selected!"
+			echo ""
+			zig_dev_release_to_check="zig-linux-x86_64-$dev_ver_string-dev.*.tar.xz"
+			worker $zig_dev_release_to_check
+			exit
+		elif [ "$option" == "2" ]; then
+			bool=false
+			echo "🍎 macOS (aarch64) selected!"
+			echo ""
+			zig_dev_release_to_check="zig-macos-aarch64-$dev_ver_string-dev.*.tar.xz"
+			worker $zig_dev_release_to_check
+			exit
+		elif [ "$option" == "3" ]; then
+			bool=false
+			echo "📺 Windows (x86_64) selected!"
+			echo ""
+			zig_dev_release_to_check="zig-windows-x86_64-$dev_ver_string-dev.*.tar.xz"
+			worker $zig_dev_release_to_check
+			exit
+		elif [ "$option" == "4" ]; then
+			bool=false
+			clear
+			exit
+		elif [ "$option" == "" ]; then
+			bool=true
+			echo "🔥Empty option! please try again🔥 "
+			echo ""
+		else
+			bool=true
+			echo "🔥Nonvalid option! please try again🔥"
+			echo ""
+		fi
+	done
 }
 
 # call the main user menu function
